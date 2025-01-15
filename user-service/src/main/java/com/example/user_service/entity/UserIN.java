10720eec7802a8b5a72572f18cb18c510152be1d
LivioGraf <livio05graf@gmail.com>
2025-01-15 19:55:06 +0100

    Implemented UserIN and UserOUT

---
+++ user-service/src/main/java/com/example/user_service/entity/UserIN.java
@@ -0,0 +1,23 @@
+package com.example.user_service.entity;
+
+public class UserIN {
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
