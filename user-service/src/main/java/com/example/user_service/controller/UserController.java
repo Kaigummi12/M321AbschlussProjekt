c790032818067138bfac6f96b5624581ef66b2b6
LivioGraf <livio05graf@gmail.com>
2025-01-15 21:05:45 +0100

    adapted controller so that it uses new hashing function (bcrypt)

--- user-service/src/main/java/com/example/user_service/controller/UserController.java
+++ user-service/src/main/java/com/example/user_service/controller/UserController.java
@@ -4,8 +4,8 @@ import com.example.user_service.entity.UserIN;
 import com.example.user_service.entity.UserOUT;
 import com.example.user_service.mapper.UserMapper;
 import com.example.user_service.model.User;
-import com.example.user_service.service.MD5HashingService;
 import com.example.user_service.service.UserService;
+import com.example.user_service.util.PasswordUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,9 +21,6 @@ public class UserController {
     @Autowired
     private UserMapper userMapper;
 
-    @Autowired
-    private MD5HashingService hashingService;
-
     @GetMapping("/{id}")
     public ResponseEntity<UserOUT> getUser(@PathVariable Long id) {
         User user = userService.getUserById(id);
@@ -35,12 +32,9 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserOUT> createUser(@RequestBody UserIN userIN) throws NoSuchAlgorithmException {
-        System.out.println("UserIn bevor hashing PW: " + userIN);
-        userIN.setPassword(hashingService.stringToMD5(userIN.getPassword()));
-        System.out.println("UserIn after hashing PW: " + userIN);
+        userIN.setPassword(PasswordUtil.hashPassword(userIN.getPassword()));
         User user = userMapper.userInToUser(userIN);
         UserOUT createdUser = userMapper.userToUserOut(userService.createUser(user));
-        System.out.println("UserOut: " + createdUser);
         return  ResponseEntity.ok().body(createdUser);
     }
 
