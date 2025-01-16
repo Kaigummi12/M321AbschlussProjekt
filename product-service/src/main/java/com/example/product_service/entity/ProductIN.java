f8d1ad7546f4ddf646ebc5f73234c07367dfa03d
livio <livio05graf@gmail.com>
2025-01-16 09:04:01 +0100

    added entity classes for ProductIN and ProductOUT

---
+++ product-service/src/main/java/com/example/product_service/entity/ProductIN.java
@@ -0,0 +1,23 @@
+package com.example.product_service.entity;
+
+public class ProductIN {
+    private String productName;
+
+    private float price;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
---
+++ product-service/src/main/java/com/example/product_service/entity/ProductOUT.java
@@ -0,0 +1,33 @@
+package com.example.product_service.entity;
+
+public class ProductOUT {
+    private Long productId;
+
+    private String productName;
+
+    private float price;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+}
